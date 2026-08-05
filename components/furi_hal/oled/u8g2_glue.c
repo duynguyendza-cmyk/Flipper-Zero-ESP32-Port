560642fe5ef7fe13b9a9a093b81ee9414aae36a0
Khang Duy <duykhangok@gmail.com>
2026-08-05 11:13:12 +0700

    okeee

--- components/furi_hal/oled/u8g2_glue.c
+++ components/furi_hal/oled/u8g2_glue.c
@@ -11,6 +11,82 @@
 
 static i2c_cmd_handle_t cmd = NULL;
 
+
+#include <driver/i2c.h>
+
+#define OLED_I2C_PORT I2C_NUM_0
+#define OLED_ADDR     0x3C
+
+static uint8_t tx_buf[33];
+static uint8_t tx_len = 0;
+static uint8_t is_cmd = 0;
+
+uint8_t u8x8_byte_hw_i2c(
+    u8x8_t *u8x8,
+        uint8_t msg,
+            uint8_t arg_int,
+                void *arg_ptr)
+                {
+                    (void)u8x8;
+
+                        switch(msg) {
+
+                            case U8X8_MSG_BYTE_INIT:
+                                    return 1;
+
+                                        case U8X8_MSG_BYTE_START_TRANSFER:
+                                                tx_len = 1;
+                                                        is_cmd = 1;
+                                                                tx_buf[0] = 0x00;   // Command mode
+                                                                        return 1;
+
+                                                                            case U8X8_MSG_BYTE_SEND:
+                                                                                {
+                                                                                        uint8_t *data = (uint8_t *)arg_ptr;
+
+                                                                                                for(uint8_t i = 0; i < arg_int; i++) {
+
+                                                                                                            if(tx_len >= sizeof(tx_buf)) {
+                                                                                                                            i2c_master_write_to_device(
+                                                                                                                                                OLED_I2C_PORT,
+                                                                                                                                                                    OLED_ADDR,
+                                                                                                                                                                                        tx_buf,
+                                                                                                                                                                                                            tx_len,
+                                                                                                                                                                                                                                pdMS_TO_TICKS(100));
+
+                                                                                                                                                                                                                                                tx_len = 1;
+                                                                                                                                                                                                                                                                tx_buf[0] = is_cmd ? 0x00 : 0x40;
+                                                                                                                                                                                                                                                                            }
+
+                                                                                                                                                                                                                                                                                        tx_buf[tx_len++] = data[i];
+                                                                                                                                                                                                                                                                                                }
+
+                                                                                                                                                                                                                                                                                                        return 1;
+                                                                                                                                                                                                                                                                                                            }
+
+                                                                                                                                                                                                                                                                                                                case U8X8_MSG_BYTE_END_TRANSFER:
+
+                                                                                                                                                                                                                                                                                                                        i2c_master_write_to_device(
+                                                                                                                                                                                                                                                                                                                                    OLED_I2C_PORT,
+                                                                                                                                                                                                                                                                                                                                                OLED_ADDR,
+                                                                                                                                                                                                                                                                                                                                                            tx_buf,
+                                                                                                                                                                                                                                                                                                                                                                        tx_len,
+                                                                                                                                                                                                                                                                                                                                                                                    pdMS_TO_TICKS(100));
+
+                                                                                                                                                                                                                                                                                                                                                                                            return 1;
+
+                                                                                                                                                                                                                                                                                                                                                                                                case U8X8_MSG_BYTE_SET_DC:
+                                                                                                                                                                                                                                                                                                                                                                                                        is_cmd = (arg_int == 0);
+                                                                                                                                                                                                                                                                                                                                                                                                                tx_len = 1;
+                                                                                                                                                                                                                                                                                                                                                                                                                        tx_buf[0] = is_cmd ? 0x00 : 0x40;
+                                                                                                                                                                                                                                                                                                                                                                                                                                return 1;
+                                                                                                                                                                                                                                                                                                                                                                                                                                    }
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                        return 0;
+                                                                                                                                                                                                                                                                                                                                                                                                                                        }
+
+
+
 uint8_t u8x8_byte_hw_i2c(
         u8x8_t* u8x8,
             uint8_t msg,
